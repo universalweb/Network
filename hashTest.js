eb2d402897e03bb62d666dde5be7fcfb387ce4ba
Thomas DeMarcii <thomasdemarcii@Thomass-MBP.fios-router.home>
2019-03-04 12:31:34 -0500

    Organize test scripts
    Ignore them for the most part. Just use the Script test folder for single file quick code tests.

--- hashTest.js
+++
@@ -1,6 +0,0 @@
-module.exports = (async () => {
-  const crypto = await require('./crypto')();
-  const hashed = crypto.hash(Buffer.from('TESTING !@##$#JHFKJHFKHKJEHLFHLKWEHJEWHKRHWLKRJHWLRJHWKJHRKWJHFKJHFKHK'));
-  const hashedOther = crypto.hash(Buffer.from('TESTING !@##$#'));
-  console.log(hashed.toString('base64'), hashedOther.toString('base64'));
-})();
