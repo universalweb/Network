2a96066e11bf7e287b616daf4f096569e9c38004
sentivate <sentivate@protonmail.com>
2019-03-10 15:23:59 -0400

    Removed lzutf8
    Swapped Lzutf8 for Brtoli

--- Script Tests/certificateCompressionText.js
+++
@@ -1,4 +0,0 @@
-const LZUTF8 = require('lzutf8');
-const input = `{"end":1649229779549,"ip":"192.168.1.1","key":"jf/Xn9sgjSVbbiyNXkmTc0TAttGFwdh/HfjQCZhOt2Q=","pad":900,"port":80,"signature":"uNH55oX7Ue027rsYSIpC12CBrutVguBj14MOWldnLx6bc0KSJ5R6VIRAXsgBkcSDWS5ZbO0pdTQ1cp/1S7dSAo2suBWSpp2LlTGW4cEyWoxEWtZW2q4uGXlm5wxsn3bC","start":1549229779549,"version":1}`;
-console.log(output.length);
-console.log(input.length);
