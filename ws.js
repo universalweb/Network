5471082822fcfb3f3895eb019e99de67b2fe7785
Erick Delgado <erickdelgado@protonmail.com>
2021-08-07 14:40:44 -0400

    file upload e.g.

--- ws.js
+++ ws.js
@@ -4,8 +4,18 @@ const app = uWS./* SSL*/App({
 	key_file_name: 'misc/key.pem',
 	cert_file_name: 'misc/cert.pem',
 	passphrase: '1234'
-}).get('/*', (res, req) => {
+}).get('/headerModificationExample', (res, req) => {
 	res.writeHeader('IsExample', 'Yes').end('Hello World!');
+}).get('/fileUploadExample', (res, req) => {
+	 res.onData((chunk, isLast) => {
+    /* Buffer this anywhere you want to */
+    console.log('Got chunk of data with length ' + chunk.byteLength + ', isLast: ' + isLast);
+
+    /* We respond when we are done */
+    if (isLast) {
+      res.end('Thanks for the data!');
+    }
+  });
 })
 	.listen(port, (token) => {
 		if (token) {
