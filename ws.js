d445e102757cb6f28cbcd9716cf6e7ce8c0806d9
sentivate <sentivate@protonmail.com>
2021-08-13 15:54:16 -0400

    uWebsockets

--- ws.js
+++ ws.js
@@ -1,4 +1,4 @@
-const uWS = require('uWebSockets.js');
+const uWS = require('./uWebSockets.js').App;
 const port = 9001;
 const {
 	utility: {
@@ -6,15 +6,12 @@ const {
 		modifyHeader
 	}
 } = state;
-const app = uWS./* SSL*/App({
-	key_file_name: 'misc/key.pem',
-	cert_file_name: 'misc/cert.pem',
-	passphrase: '1234'
-}).get('/headerModificationExample', (res, req) => {
-	modifyHeader('IsExample', 'Yes').end('Hello world')
-}).get('/fileUploadExample', (res, req) => {
-	asyncFileReadandWrite(res, __dirname)
+const app = uWS({}).get('/headerModificationExample', (res, req) => {
+	modifyHeader('IsExample', 'Yes').end('Hello world');
 })
+	.get('/fileUploadExample', (res, req) => {
+		asyncFileReadandWrite(res, __dirname);
+	})
 	.listen(port, (token) => {
 		if (token) {
 			console.log(`Listening to port ${port}`);
