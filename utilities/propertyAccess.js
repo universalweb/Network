7a1283b85784c5e5dc29e6e09f405313b0463300
Universal Web <code@universalweb.io>
2023-03-18 07:51:25 -0400

    PropertyAccess Patch
    Part of Client Merge Part 2

--- utilities/propertyAccess.js
+++ utilities/propertyAccess.js
@@ -1,37 +1,31 @@
-module.exports = (state) => {
-	const {
-		utility: {
-			apply,
-			hasValue,
-			mapWhile,
+import {
+	apply,
+	hasValue,
+	mapWhile,
+} from 'Acid';
+const regEx = /^([a-zA-Z0-9.]+)$/;
+const sensitiveWords = /(constructor|prototype|window|self|top|alert|confirm|eval|function|Object|Array)/;
+const errorNoneExistentProperty = 'None existent property';
+const errorPropertyStringAccess = 'Property string access';
+export function isPropertyValid(string) {
+	return regEx.test(string) && !sensitiveWords.test(string);
+}
+function goDownTree(currentPosition, item) {
+	if (apply(Object.prototype.hasOwnProperty, currentPosition, [item])) {
+		return true;
+	}
+}
+export function getPropertyFromString(string, objectMain, error) {
+	let object = objectMain;
+	const arraySplit = string.split('.');
+	mapWhile(arraySplit, (item) => {
+		if (goDownTree(object, item)) {
+			object = object[item];
+		} else {
+			error(errorPropertyStringAccess, errorNoneExistentProperty, string);
+			object = false;
+			return !hasValue(object);
 		}
-	} = state;
-	const regEx = /^([a-zA-Z0-9.]+)$/;
-	const sensitiveWords = /(constructor|prototype|window|self|top|alert|confirm|eval|function|Object|Array)/;
-	const isPropertyValid = (string) => {
-		return regEx.test(string) && !sensitiveWords.test(string);
-	};
-	const goDownTree = (currentPosition, item) => {
-		if (apply(Object.prototype.hasOwnProperty, currentPosition, [item])) {
-			return true;
-		}
-	};
-	const errorNoneExistentProperty = 'None existent property';
-	const errorPropertyStringAccess = 'Property string access';
-	const getPropertyFromString = (string, objectMain, error) => {
-		let object = objectMain;
-		const arraySplit = string.split('.');
-		mapWhile(arraySplit, (item) => {
-			if (goDownTree(object, item)) {
-				object = object[item];
-			} else {
-				error(errorPropertyStringAccess, errorNoneExistentProperty, string);
-				object = false;
-				return !hasValue(object);
-			}
-		});
-		return object;
-	};
-	state.utility.isPropertyValid = isPropertyValid;
-	state.utility.getProperty = getPropertyFromString;
-};
+	});
+	return object;
+}
