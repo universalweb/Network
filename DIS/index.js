faa7d8b66a5fb22218b48d907d8a3f2cfb0627c8
Universal Web <code@universalweb.io>
2023-03-23 21:35:06 -0400

    DIS Api converted

--- DIS/index.js
+++ DIS/index.js
@@ -1,19 +1,15 @@
-module.exports = async () => {
-	const server = await require('../server/index.js')({
-		maxMTU: 1100,
-		max: 900,
-		// DIS server Domain certificate to be loaded used for connection encryption
-		profile: `${__dirname}/../services/universal.web.cert`,
-		// Where to load app resources from
-		resourceDirectory: `${__dirname}/resources`,
-		// DIS Server ID used for load balancing and attaching to the end of connection IDs
-		id: '0',
-		onConnectMessage: `Welcome to the Universal Web.`,
-		port: 8888
-	});
-	const {
-		status,
-		cnsl,
-	} = server;
-	cnsl('DIS Status', status);
-};
+import { createServer } from '../server/index.js';
+import { info } from 'utilities/logs.js';
+const server = await createServer({
+	maxMTU: 1100,
+	max: 900,
+	// DIS server Domain certificate to be loaded used for connection encryption
+	profile: `${__dirname}/../services/universal.web.cert`,
+	// Where to load app resources from
+	resourceDirectory: `${__dirname}/resources`,
+	// DIS Server ID used for load balancing and attaching to the end of connection IDs
+	id: '0',
+	onConnectMessage: `Welcome to the Universal Web.`,
+	port: 8888
+});
+info('DIS Server', server);
