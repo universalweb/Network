1b4c897cf0feee281af89a9714080792d5230fba
Universal Web <code@universalweb.io>
2023-04-12 04:41:13 -0400

    Quick patch for server testing HTTP benchmarking

--- scripts/http/index.js
+++ scripts/http/index.js
@@ -1,8 +1,8 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-const http = require('https');
+import http from 'https';
 const hostname = '127.0.0.1';
 const port = 8888;
-const fs = require('fs');
+import fs from 'fs';
 const options = {
 	key: fs.readFileSync('/home/main/MEGAsync/Github/Network/scripts/http/key.pem'),
 	cert: fs.readFileSync('/home/main/MEGAsync/Github/Network/scripts/http/cert.pem')
@@ -10,26 +10,14 @@ const options = {
 const server = http.createServer(options, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end(`(async () => {
-	console.log('TESTING');
-	await app.view.set('pageTitle', 'DEMO SITE');
-	const state = app.component({
-		name: 'exampleApp',
-		template: '<h1>HELLO WORLD</h1><p>The first UW Website using UDSP</p>'
-	});
-	console.log(state);
-	await app.view.set('components.main.exampleApp', true);
-})();
-`);
+	res.end(`TESTING`);
 });
 server.listen(port, hostname, () => {
 	console.log(`Server running at https://${hostname}:${port}/`);
 });
-(async () => {
-	const fetch = require('node-fetch');
-	console.time('http');
-	const response = await fetch('https://127.0.0.1:8888');
-	const body = await response.text();
-	console.timeEnd('http');
-	console.log(body);
-})();
+import fetchIt from 'node-fetch';
+console.time('http');
+const response = await fetchIt('https://127.0.0.1:8888');
+const body = await response.text();
+console.timeEnd('http');
+console.log(body);
--- serverApp/index.js
+++ serverApp/index.js
@@ -1,21 +1,36 @@
 /*
 		* App server example
 */
-import { createServer } from '../server/index.js';
+import { createServer } from 'server/index.js';
 import { info } from 'utilities/logs.js';
 const appServer = await createServer({
+	// realtime mode - to keep the connection alive for bidirectional communication
+	realtime: true,
+	// Source Verification to ensure that data coming from a client is coming from that source
+	sourceVerification: true,
+	// Max packet retries for a singular request before restarting the request
+	maxPacketRetries: 3,
+	// Max retries for a singular request before giving up
+	maxRequestRetries: 3,
+	// Max retries for a singular request before giving up
+	maxResponseRetries: 3,
 	// Max size of packets
-	maxMTU: 1100,
+	maxPacketSize: 1100,
+	// Max size of a message
+	maxMessageSize: 10000,
 	// Max size of body and head data sections in packets
 	maxPayloadSize: 1000,
-	max: 900,
+	// max data size in a singular packet
+	maxFileSize: 900,
 	// Domain certificate to be loaded used for connection encryption
 	profile: `${__dirname}/../services/universal.web.cert`,
 	// Where to load app resources from
 	resourceDirectory: `${__dirname}/resources`,
 	// Server ID used for load balancing and attaching to the end of connection IDs
 	id: '0',
+	// on connect message to respond with when a connection is established
 	onConnectMessage: `Welcome to the Universal Web.`,
+	// Port to listen on for connections
 	port: 8888
 });
 info('App Server Status', appServer);
