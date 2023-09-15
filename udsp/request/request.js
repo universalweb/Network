4dc0557b00fe528e170bac017cdd5952c4cdcd81
Universal Web <code@universalweb.io>
2023-09-15 11:20:14 -0400

    Splitting Ask Class into Request/Response Classes

---
+++ udsp/request/request.js
@@ -0,0 +1,51 @@
+import {
+	promise, assign, omit, eachArray, stringify, get, isBuffer, isPlainObject, isArray, isMap, construct, each, hasLength, hasValue, UniqID
+} from '@universalweb/acid';
+import { decode, encode } from 'msgpackr';
+import {
+	failed, info, msgReceived, msgSent
+} from '#logs';
+import { Base } from './base.js';
+import { request } from '#udsp/requestMethods/request';
+export class Ask extends Base {
+	constructor(method = 'get', path, parameters, data, head, options = {}, source) {
+		super(options, source);
+		const {
+			requestQueue,
+			streamIdGenerator,
+		} = source;
+		console.log('Ask', data);
+		const methodSanitized = method.toLowerCase();
+		this.request.method = methodSanitized;
+		this.method = methodSanitized;
+		if (path) {
+			this.request.path = path;
+			this.path = path;
+		}
+		if (parameters) {
+			this.request.parameters = parameters;
+			this.parameters = parameters;
+		}
+		if (data) {
+			this.request.data = data;
+		}
+		if (head) {
+			this.request.head = head;
+		}
+	}
+	completeReceived() {
+		console.log('Ask complete', this);
+		if (this.state === 3) {
+			this.state = 4;
+		}
+		this.readyState = 4;
+		this.flush();
+		this.accept(this);
+	}
+	isRequest = true;
+	type = 'request';
+	request = {};
+}
+export async function ask(source) {
+	return construct(Ask, omit);
+}
---
+++ udsp/request/response.js
@@ -0,0 +1,6 @@
+export class UWResponse {
+	constructor(headers, data) {
+	}
+	isResponse = true;
+	type = 'response';
+}
