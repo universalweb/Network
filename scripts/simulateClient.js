c219453ba889de7554c3ceda6e8547c58aed7071
Tomek Marchi <tom@universalweb.io>
2023-11-10 03:26:25 -0500

    Re-Enable Demo new paths

--- scripts/simulateClient.js
+++ scripts/simulateClient.js
@@ -8,11 +8,11 @@ import { client } from '#udsp';
 console.time('Connected');
 // Universal Web Socket
 const uwClient = await client({
-	destinationCertificate: `${currentPath(import.meta)}/../services/universal.web-EphemeralPublic.cert`,
+	destinationCertificate: `${currentPath(import.meta)}/../serverApp/certs/universal_web-Ephemeral/universal.web-EphemeralPublic.cert`,
 	// Load Profile Certificate from Keychain
 	// keychain: 'Universal Web Profile',
 	// Load Profile Certificate from file
-	profile: `${currentPath(import.meta)}/../profiles/default-Ephemeral.cert`
+	profile: `${currentPath(import.meta)}/../profiles/default/default-Ephemeral/default-Ephemeral.cert`
 });
 // const connection = await uwClient.connect();
 //  await client('universalweb.io', {keychain: 'Universal Web Profile'});
