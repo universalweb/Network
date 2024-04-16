48b2a197f12c16cc009e83a1f9adb70f20fb37c3
Tomek Marchi <tommarchi@protonmail.com>
2024-04-16 05:14:01 -0400

    Update certificates.js and index.js files

--- scripts/certificates.js
+++ scripts/certificates.js
@@ -1,39 +1,34 @@
-import { DomainCertificate, createDomainCertificate, createProfile } from '#udsp/certificate/index';
+import { DomainCertificate, createDomainCertificate, uwProfile } from '#udsp/certificate/index';
 import { currentPath } from '@universalweb/acid';
 import { decode } from '#utilities/serialize';
 const dirname = currentPath(import.meta);
-const profile = new UWProfile();
+const profile = uwProfile();
 profile.save('profile', `${dirname}/../profiles/default`);
 const domainCertificate = new DomainCertificate({
 	entity: 'universalweb.io',
-	owner: profile.getSignature(),
+	backupHash: profile.getSignature(),
 	records: [
 		[
 			'aaaa',
 			'@',
 			'::1',
 			8888,
-			0, // PRIORITY
-			'27.950575 -82.457176' // GEOLOCATION FEATURE
+			// PRIORITY
+			0,
+			// GEOLOCATION FEATURE
+			'27.950575 -82.457176'
 		],
 		[
 			'a',
 			'@',
 			'127.0.0.1',
 			8888,
-			0, // PRIORITY
-			'27.950575 -82.457176' // GEOLOCATION FEATURE
+			// PRIORITY
+			0,
+			// GEOLOCATION FEATURE
+			'27.950575 -82.457176'
 		],
 	],
 });
 domainCertificate.save('universal.web', `${dirname}/../serverApp/certs`);
 console.log(domainCertificate);
-const profile = await createProfile({
-	savePath: `${dirname}/../profiles/default`,
-	folder: 'default',
-	certificateName: 'default',
-	saveToKeychain: {
-		account: 'Default Profile'
-	}
-});
-console.log('Profile created (Master & IDENTITY CERTIFICATEs)', decode(domainProfile.ephemeral.certificate));
--- udsp/certificate/index.js
+++ udsp/certificate/index.js
@@ -1,59 +1,8 @@
-import { convertToDomainCertificateObject, createDomainCertificateObject, objectToRawDomainCertificate } from './domain.js';
-import { convertToProfileCertificateObject, createProfileCertificateObject, objectToRawProfileCertificate } from './profile.js';
 import { decode, encode } from '#utilities/serialize';
 import { imported, logCert } from '#logs';
 import { read, write } from '#file';
 import { assign } from '@universalweb/acid';
 // Add certificate verification via DIS
-export async function verifyCertificate(parentCertificate, childCertificate) {
-	logCert(parentCertificate, childCertificate);
-}
-export async function getCertificate(filepath) {
-	logCert('Get => ', filepath);
-	const original = await read(filepath);
-	if (original) {
-		return decode(original);
-	} else {
-		logCert('FAILED TO GET CERT', filepath);
-	}
-}
-export async function loadCertificate(filepath) {
-	logCert('Get => ', filepath);
-	const certificate = await read(filepath);
-	if (certificate) {
-		return certificate;
-	} else {
-		logCert('FAILED TO LOAD CERT', filepath);
-	}
-}
-export async function parseCertificate(filepath) {
-	logCert('Get => ', filepath);
-	const certificate = await getCertificate(filepath);
-	if (certificate) {
-		const [type,] = certificate;
-		if (type === 0) {
-			return convertToDomainCertificateObject(certificate);
-		} else if (type === 1) {
-			return convertToProfileCertificateObject(certificate);
-		}
-		return certificate;
-	} else {
-		logCert('FAILED TO PARSE CERT', filepath);
-	}
-}
-export async function createCertificate(config, options) {
-	const { type } = config;
-	if (type) {
-		if (type === 0) {
-			const certObject = createDomainCertificateObject(config, options);
-			return objectToRawDomainCertificate(certObject);
-		} else if (type === 1) {
-			const certObject = createProfileCertificateObject(config, options);
-			return objectToRawProfileCertificate(certObject);
-		}
-		console.log('Certificate Type Incorrect');
-	} else {
-		console.log('Certificate Type Required');
-	}
-}
-export * from './save.js';
+export * from './domain.js';
+export * from './domain.js';
+export * from './profile.js';
