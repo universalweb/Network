ef55d35b8411fc4feff97a9662564d93baa96086
Tomek Marchi <tommarchi@protonmail.com>
2025-04-30 07:52:45 -0400

    Viat Defaults updated

--- viat/defaults.js
+++ viat/defaults.js
@@ -1,12 +1,14 @@
 const version = 1;
 const coinName = 'VIAT';
-const coinAlias = 'V';
+const coinNamePlural = 'VIAT';
 const coinSymbol = '⩝';
 const coinSmallestUnitName = 'units';
 const coinLargestUnitName = 'VIAT';
 const coinTicker = 'VIAT';
 const coinUnitName = 'VIAT';
 const coinElementName = 'Vitanium';
+const coinElementSymbol = 'Vi';
+const vFED = 'Viat Federal Reserve';
 // whole:8  decimal:56|69 total:64|77
 // Used for -> for micro payments, nano transactions, quantum payments (ultra-fine granularity transactions), High-Frequency Trading Fees, Low-Value Use Cases, Layer-2 or Batching, Counterparty Compatibility, atomic swaps, Viat Federal Reserve Deflationary Mechanism
 const coinDecimalPlaces = 69;
@@ -19,7 +21,10 @@ const coinMaxWholeSupplyLength = coinMaxWholeSupplyDisplay.length;
 const coinMaxSupplyInt = BigInt(coinMaxSupply);
 const coinMaxSupplyParsed = '52,000,000.0'.padEnd(coinDecimalPlaces, '0');
 const viatDefaults = {
-	coinAlias,
+	coinElementName,
+	coinElementSymbol,
+	vFED,
+	coinNamePlural,
 	coinDecimalPlaces,
 	coinDigitCount,
 	coinMaxSupply,
